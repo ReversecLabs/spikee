08b58b42c637a4a144f3f163268d92084b210853
ThomasCross <thomas.cross@reversec.com>
2026-01-15 18:16:55 +0000

    feat: Add multi-file and analysis to result-viewer

--- spikee/viewer.py
+++ spikee/viewer.py
@@ -6,9 +6,11 @@ import hashlib
 from selenium import webdriver
 
 from spikee.utilities.files import process_jsonl_input_files, read_jsonl_file, extract_resource_name
+from spikee.utilities.results import ResultProcessor
 
 
 VIEWER_NAME = "SPIKEE Viewer"
+TRUNCATE_LENGTH = 500
 
 
 def create_viewer(viewer_folder, results, host, port) -> Flask:
@@ -18,11 +20,14 @@ def create_viewer(viewer_folder, results, host, port) -> Flask:
         static_folder=os.path.join(viewer_folder, "static"),
         template_folder=os.path.join(viewer_folder, "templates"),
     )
-
-    loaded_file = [list(results.keys())[0]] if results else []
-    loaded_results = {}
+    loaded_file = ["combined"]
+    loaded_results = {'combined': {}}
+    result_processors = {}
+    combined_results = {}
     for name, entries in results.items():
         loaded_results[name] = {}
+
+        # Attempt to parse 'response' field as JSON
         for entry in entries:
             backup = entry['response']
             try:
@@ -31,6 +36,11 @@ def create_viewer(viewer_folder, results, host, port) -> Flask:
                 entry['response'] = backup
 
             loaded_results[name][str(entry['id'])] = entry
+            loaded_results['combined'][str(name + "-" + str(entry['id']))] = entry
+
+        # Create ResultProcessor for this results file
+        result_processors[name] = ResultProcessor(results=entries, result_file=name).generate_output()
+    result_processors["combined"] = ResultProcessor(results=loaded_results['combined'].values(), result_file="combined").generate_output()
 
     # Context Processor (Allows templates to run functions)
     @viewer.context_processor
@@ -39,17 +49,28 @@ def create_viewer(viewer_folder, results, host, port) -> Flask:
             """Return the name of the viewer application."""
             return VIEWER_NAME
 
-        def get_loaded_result_file():
-            """Return the name of the results file."""
-            return loaded_file[0]
-
         def get_loaded_results_data(name: str):
             """Return the results data."""
-            return loaded_results.get(name, {}).values()
+            return loaded_results.get(name, {})
 
         def get_result_files():
             """Return the available results files."""
             return list(loaded_results.keys())
+
+        def get_result_processor(name: str):
+            """Return the result processor output."""
+            return result_processors.get(name)
+
+        def process_output(output: str, truncated: bool = False) -> str:
+            """Process output string for display."""
+
+            if output is None:
+                return "—"
+
+            elif truncated and len(output) > TRUNCATE_LENGTH:
+                return output[:TRUNCATE_LENGTH] + "...[Truncated]"
+
+            return output
 
         def string_to_colour(string: str) -> str:
             """
@@ -73,11 +94,17 @@ def create_viewer(viewer_folder, results, host, port) -> Flask:
 
         return dict(
             get_app_name=get_app_name,
-            get_loaded_result_file=get_loaded_result_file,
             get_loaded_results_data=get_loaded_results_data,
             get_result_files=get_result_files,
+            get_result_processor=get_result_processor,
+            process_output=process_output,
             string_to_colour=string_to_colour
         )
+
+    @viewer.before_request
+    def before_request_func():
+        loaded_file[0] = request.args.get('result_file', 'combined')
+        viewer.jinja_env.globals['loaded_file'] = loaded_file[0]
 
     @viewer.route("/")
     def index():
@@ -85,7 +112,7 @@ def create_viewer(viewer_folder, results, host, port) -> Flask:
 
     @viewer.route("/file/")
     def result_file():
-        return render_template("result_file.html")
+        return render_template("result_file.html", truncated=True)
 
     @viewer.route("/entry/<entry>")
     def result_entry(entry):
@@ -93,16 +120,7 @@ def create_viewer(viewer_folder, results, host, port) -> Flask:
         if not entry_data:
             abort(404, description="Entry not found")
 
-        return render_template("result_entry.html", entry=entry_data)
-
-    @viewer.route("/load_results/")
-    def load_results():
-        name = request.args.get("result_file")
-        if name not in results:
-            abort(404, description="Results file not found")
-
-        loaded_file[0] = name
-        return url_for("index")
+        return render_template("result_entry.html", id=entry, entry=entry_data)
 
     @viewer.route("/entry/<entry>/card")
     def result_card(entry):
@@ -110,7 +128,7 @@ def create_viewer(viewer_folder, results, host, port) -> Flask:
         if not entry_data:
             abort(404, description="Entry not found")
 
-        return render_template("download.html", entry=entry_data, download=True)
+        return render_template("download.html", id=entry, entry=entry_data, download=True)
 
     @viewer.route("/entry/<entry>/download")
     def result_to_image(entry):
@@ -122,7 +140,7 @@ def create_viewer(viewer_folder, results, host, port) -> Flask:
 
         driver = webdriver.Chrome(options=options)
         try:
-            driver.get(f"http://{host}:{port}/entry/{entry}/card")  # Dummy URL
+            driver.get(f"http://{host}:{port}/entry/{entry}/card?result_file={loaded_file[0]}")  # Dummy URL
             img_bytes = driver.get_screenshot_as_png()
         finally:
             driver.quit()
@@ -132,7 +150,7 @@ def create_viewer(viewer_folder, results, host, port) -> Flask:
             BytesIO(img_bytes),
             mimetype='image/png',
             as_attachment=True,
-            download_name=f"{loaded_file}_{entry}.png"
+            download_name=f"{loaded_file[0]}_{entry}.png"
         )
 
     return viewer
@@ -149,7 +167,8 @@ def run_viewer(args):
         name = extract_resource_name(result_file)
         results[name] = read_jsonl_file(result_file)
 
-        print(f"[Info] Loaded {len(results[name])} entries from results file: {result_file}")
+    print("[Overview] Analyzing the following file(s): ")
+    print(" - " + "\n - ".join(results_files))
 
     viewer_folder = os.path.join(os.getcwd(), "viewer")
     if not os.path.isdir(viewer_folder):
