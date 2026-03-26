8862703111b97e75e8e978ef9f5b9a1bee28e742
ThomasCross <thomas.cross@reversec.com>
2026-03-26 14:55:54 +0000

    change: optional module functions, now defaults

--- spikee/templates/module.py
+++ spikee/templates/module.py
@@ -1,15 +1,13 @@
 from spikee.utilities.enums import ModuleTag
 
-from abc import ABC, abstractmethod
+from abc import ABC
 from typing import List, Tuple
 
 
 class Module(ABC):
-    @abstractmethod
     def get_description(self) -> Tuple[List[ModuleTag], str]:
-        return [], "Blank Description"
+        return [], "No Module description available."
 
-    @abstractmethod
     def get_available_option_values(self) -> Tuple[List[str], bool]:
         """Return supported attack options; Tuple[options (default is first), llm_required]
         e.g., (["mode=aggressive"], True) - Had an option mode, and requires llm 'model' to operate."""
